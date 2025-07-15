816ea1c2d59287b09cd19ba324d927bb79b0b488
Italo Teles <italoteles399@gmail.com>
2025-07-14 23:43:54 -0300

    feat: adiciona middleware para usuarios anonimos

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -6,11 +6,13 @@ import { categoriesRoutes } from "./categories.route.js";
 import { productsRoutes } from "./products.route.js";
 import { paymentMethodRoutes } from "./payment-methods.route.js";
 import { orderRoutes } from "./orders.route.js";
+import { allowAnonymousUser } from "../middlewares/allow-anonymous-user.middleware.js";
 
 // implementa as rotas de routes (rota localhost:3000/user ou localhost:3000/auth/login por exemplo)
 
 export const routes = (app: express.Express) => {
     app.use(express.json({ limit: "5mb" }));
+    app.use(allowAnonymousUser);
     app.use(authRoutes); // rota de autenticação de preferência em primeiro plano
     app.use(userRoutes);
     app.use(companyRoutes);
