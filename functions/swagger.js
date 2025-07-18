1dfa7f5a19e33a36f2b814a4b6fad4e74d34650a
Italo Teles <italoteles399@gmail.com>
2025-07-18 07:42:48 -0300

    refatoracao: atualizado configuração do Swagger para OpenApi 3.0.0

--- functions/swagger.js
+++ functions/swagger.js
@@ -5,9 +5,13 @@ const doc = {
         title: "E-commerce API",
         description: "API para gestão de dados do e-commerce",
     },
-    host: "127.0.0.1:5001/api-ecommerce-351ee/us-central1",
-    basePath: "/api",
-    schemes: ["https"],
+
+    servers: [
+        {
+            url: "http://127.0.0.1:5001/api-ecommerce-351ee/us-central1/api",
+            description: "dev",
+        },
+    ],
 };
 
 const outputFile = "./src/docs/swagger-output.json";
@@ -16,4 +20,4 @@ const routes = ["./src/routes/index.ts"];
 /* NOTE: If you are using the express Router, you must pass in the 'routes' only the 
 root file where the route starts, such as index.js, app.js, routes.js, etc ... */
 
-swaggerAutogen()(outputFile, routes, doc);
+swaggerAutogen({ openapi: "3.0.0" })(outputFile, routes, doc);
